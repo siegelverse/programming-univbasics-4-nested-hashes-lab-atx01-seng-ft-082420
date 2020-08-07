c69a31b1f70c6ebdfd34b604324acbea29dfe87b
Marcus Siegel <msiegel37@gmail.com>
2020-08-07 22:20:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-    programmer_hash[:dennis_ritchie][:known_for][:languages]
+    programmer_hash[:dennis_ritchie][:known_for] :languages]
 end
 
 def adding_matz
