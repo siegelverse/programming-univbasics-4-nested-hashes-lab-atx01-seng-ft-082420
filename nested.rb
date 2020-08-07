8195fedd7db61ba9143493acc7c94fa9ff22d94a
Marcus Siegel <msiegel37@gmail.com>
2020-08-07 23:01:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-  programmer_hash[:yukihiro_matsumoto]
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", 
 end
 
 def changing_alan
